fdd66b63471c0d8e3024fc88d19b44243054ae6b
« Ben <benybenmo@gmail.com>
2023-07-11 10:56:17 -0700

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
-"""Load, add, save"""
+from json import dumps
 
 
-from sys import argv
-from os.path import exists
-
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-namefile = "add_item.json"
-argc = len(argv)
-
-new_list = []
-
-if exists(namefile):
-    new_list = load_from_json_file(namefile)
-
-if (argc == 1):
-    save_to_json_file([], namefile)
-else:
-    for i in range(1, argc):
-        new_list.append(argv[i])
-    save_to_json_file(new_list, namefile)
+def save_to_json_file(my_obj, filename):
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(dumps(my_obj))
